8522acecbde2bf9d995bfbeed532124307b99228
omar algalfy <algalfy71@gmail.com>
2022-07-03 16:56:32 +0200

    requests done

--- app/modules/customers/Http/Controllers/backend/Customers.php
+++ app/modules/customers/Http/Controllers/backend/Customers.php
@@ -3,10 +3,11 @@
 namespace Customers\Http\Controllers\backend;
 
 use App\Http\Controllers\Controller;
+use Customers\Http\Requests\Backend\store;
 
 class Customers extends Controller
 {
-    public function index()
+    public function index(store $request)
     {
        return loadView('customers',"backend","index");
     }
--- app/modules/customers/Http/Controllers/frontend/Customers.php
+++ app/modules/customers/Http/Controllers/frontend/Customers.php
@@ -3,10 +3,11 @@
 namespace Customers\Http\Controllers\frontend;
 
 use App\Http\Controllers\Controller;
+use Customers\Http\Requests\Frontend\store;
 
 class Customers extends Controller
 {
-    public function index()
+    public function index(store $request)
     {
         // return view("customers::frontend.index");
         return loadView('customers',"frontend","index");
